e362a3c478ea35f736cf78e4b9bcaca28e36171e
MeAmarP <appotdar05@gmail.com>
2024-01-19 02:43:43 +0530

    Add FunctionTool and ReActAgent imports, and get_today_date method to noprobllema class

--- sample_gradio.py
+++ sample_gradio.py
@@ -2,11 +2,19 @@
 from llama_index.llms import Ollama
 from llama_index.llms import ChatMessage
 
+from llama_index.tools import FunctionTool
+from llama_index.agent import ReActAgent
+
+from datetime import datetime
+
 import gradio as gr
 
 class noprobllema():
     def __init__(self):
         self.llm = Ollama(model="llama2")
+        function_tool = FunctionTool.from_defaults(fn=self.get_today_date)
+        self.tools = [function_tool]
+        
         
 
     def chat(self, message, history):
@@ -17,6 +25,13 @@ class noprobllema():
         # TODO : Use streamimg to get the response
         resp_bot = self.llm.chat(msg)
         return resp_bot.dict()['message']['content']
+    
+
+
+    
+
+
+    
 
 run_bot = noprobllema()
 demo = gr.ChatInterface(run_bot.chat, title="NoProbLlama! Intelligent AI Chatbot")
